9eccf954b13df0691878a90dc314bb95fe1c484b
ZensykU2 <crto.CT@gmail.com>
2025-11-18 14:14:05 +0100

    feat: backends starting through config.json

--- backend/backend.go
+++ backend/backend.go
@@ -3,29 +3,21 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8081"
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "backend answer from %s: %s\n", r.Host, r.URL.Path)
+		fmt.Fprintf(w, "Backend (%s): %s\n", port, r.URL.Path)
 	})
 
-	// Server 1 (Port 8081)
-	go func() {
-		fmt.Println("backend running on :8081")
-		if err := http.ListenAndServe(":8081", nil); err != nil {
-			fmt.Println("Error on port 8081:", err)
-		}
-	}()
-
-	// Server 2 (Port 8082)
-	go func() {
-		fmt.Println("Backend running on :8082")
-		if err := http.ListenAndServe(":8082", nil); err != nil {
-			fmt.Println("Error on port 8082:", err)
-		}
-	}()
-
-	// Blocking so the program doesn't end.
-	select {}
+	fmt.Printf("Backend läuft auf :%s\n", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		fmt.Printf("Fehler auf Port %s: %v\n", port, err)
+	}
 }
